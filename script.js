415602e4529d21eb499cdb1020c6da31a36acff2
Agustin Gallardo <agustingallardo97@outlook.com.ar>
2026-05-22 17:45:20 -0300

    modificacion de script.js funcionando

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const caja = document.getElementById('caja')
 function funcion(){
-    alert('[DEBUG] Sejecutando cambio de imagen')
+    alert('[DEBUG] ejecutando cambio de imagen')
     const newImg = document.createElement('img')
     newImg.src = 'pato.png'
     caja.replaceWith(newImg)
