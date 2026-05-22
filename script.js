aa76c695c6360bff702dda28cef822413e52dfa4
agus14gc14 <103479465+agus14gc14@users.noreply.github.com>
2026-05-22 05:30:13 -0300

    modificacion de parametros de imagen

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("caja").onclick = function(){
     const newImg = document.createElement('img')
-    newImg.src = "pato.png"
+    newImg.src = 'pato.png'
     document.getElementById("caja").replaceWith(newImg)
 }
