972b3f940ae0042e70e63be42a6419d1db89d2ac
agus14gc14 <103479465+agus14gc14@users.noreply.github.com>
2026-05-22 05:20:15 -0300

    modificaciones en html + script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("caja").onclick = function(){
     const newImg = document.createElement('img')
-    newImg.src = ("")
-    document.getElementById("caja").replaceWith
+    newImg.src = ("pato.png")
+    document.getElementById("caja").replaceWith(newImg)
 }
