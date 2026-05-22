a49071bc482547a1573e10150eaec02c1f496a0b
Agustin Gallardo <agustingallardo97@outlook.com.ar>
2026-05-22 18:06:34 -0300

    cambios finales

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const caja = document.getElementById('caja')
 function funcion(){
-    alert('[DEBUG] ejecutando cambio de imagen')
     const newImg = document.createElement('img')
+    const newP = document.createElement('p')
     newImg.src = 'poster.jpg'
     newImg.setAttribute('width', '800')
     newImg.setAttribute('height', '600')
+    document.getElementById('mensaje').innerHTML='Te queremos mucho Caro, Ambar y Agus.'
     caja.replaceWith(newImg)
 }
 
