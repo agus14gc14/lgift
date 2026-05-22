f742995c9baf1cee45aceb58296dcd0f515ce70e
Agustin Gallardo <agustingallardo97@outlook.com.ar>
2026-05-22 17:56:20 -0300

    modificacion script newImg

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function funcion(){
     alert('[DEBUG] ejecutando cambio de imagen')
     const newImg = document.createElement('img')
     newImg.src = 'poster.jpg'
+    newImg.setAttribute('width', '800')
+    newImg.setAttribute('height', '600')
     caja.replaceWith(newImg)
 }
 
